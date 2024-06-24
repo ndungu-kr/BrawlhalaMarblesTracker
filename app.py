47ff92f26483bbefbf275fa3463a289fa899ea78
ndungu-kr <85244705+ndungu-kr@users.noreply.github.com>
2024-06-24 13:55:02 +0100

    switching to blueprints

--- app.py
+++ app.py
@@ -1,10 +1,6 @@
-from flask import Flask, render_template
+from app import create_app
 
-app = Flask(__name__)
-
-@app.route('/')
-def home():
-    return render_template('home.html')
+app = create_app()
 
 if __name__ == '__main__':
     app.run(debug=True)
