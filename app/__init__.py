6b561f2c0ed3a5f4204944637e5f6564c8a7286a
ndungu-kr <85244705+ndungu-kr@users.noreply.github.com>
2024-06-26 12:20:55 +0100

    initializing database

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,18 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
 
 def create_app():
     app = Flask(__name__)
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///marbles.db'
+    db.init_app(app)
+
+    from . import models
+
+    with app.app_context():
+        db.create_all()
+        print("Database running...")
 
     from .routes import main
     app.register_blueprint(main)
