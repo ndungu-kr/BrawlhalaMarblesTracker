c1c57ce46d5bcaeced06dd65ba74e7d2dad28a9f
ndungu-kr <85244705+ndungu-kr@users.noreply.github.com>
2024-06-26 16:35:00 +0100

    Added base template and files for login and sign up pages

--- app/auth.py
+++ app/auth.py
@@ -1,10 +1,10 @@
-from flask import Blueprint
+from flask import Blueprint, render_template
 
 auth = Blueprint('auth', __name__)
 
 @auth.route('/login')
 def login():
-    return "<p>Login</p>"
+    return render_template('login.html')
 
 
 @auth.route('/logout')
@@ -14,5 +14,5 @@ def logout():
 
 @auth.route('/sign-up')
 def sign_up():
-    return "<p>Sign Up</p>"
+    return render_template('sign_up.html')
 
