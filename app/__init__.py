fc80d449659456f500be13153547ec8d49604d74
ndungu-kr <85244705+ndungu-kr@users.noreply.github.com>
2024-06-26 12:34:54 +0100

    Handling db migrations

--- app/__init__.py
+++ app/__init__.py
@@ -1,18 +1,17 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 db = SQLAlchemy()
+migrate = Migrate()
 
 def create_app():
     app = Flask(__name__)
     app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///marbles.db'
     db.init_app(app)
+    migrate.init_app(app, db)
 
     from . import models
-
-    with app.app_context():
-        db.create_all()
-        print("Database running...")
 
     from .routes import main
     app.register_blueprint(main)
