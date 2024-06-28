74de91934a5c14b45fb06c68efbf9317003b3ac7
ndungu-kr <85244705+ndungu-kr@users.noreply.github.com>
2024-06-28 13:46:05 +0100

    getting rid of migrations for now

--- app/__init__.py
+++ app/__init__.py
@@ -1,25 +1,27 @@
 from os import getenv
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from flask_migrate import Migrate
+from os import path
 
 db = SQLAlchemy()
-migrate = Migrate()
+DB_NAME = 'marbles.db'
 
 def create_app():
     app = Flask(__name__)
     app.config["SECRET_KEY"] = getenv('DB_SECRET_KEY')
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///marbles.db'
-
+    app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
     db.init_app(app)
-    migrate.init_app(app, db)
-
-    from . import models
 
     from .routes import main
     from .auth import auth
 
     app.register_blueprint(main, url_prefix='/')
     app.register_blueprint(auth, url_prefix='/')
+
+    from . import models
+
+    with app.app_context():
+        db.create_all()
+        print('Database Running...')
 
     return app
