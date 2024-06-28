37f9b4e3f429630fe3fd25b24369a57e90277cf4
ndungu-kr <85244705+ndungu-kr@users.noreply.github.com>
2024-06-28 15:51:07 +0100

    user log in functionality

--- app/auth.py
+++ app/auth.py
@@ -8,7 +8,22 @@ auth = Blueprint('auth', __name__)
 
 @auth.route('/login', methods=['GET', 'POST'])
 def login():
-    return render_template('login.html')
+    if request.method == "POST":
+        email = request.form.get("email")
+        password = request.form.get("password")
+        user = User.query.filter_by(email=email).first()
+
+        if user:
+            if check_password_hash(user.password, password):
+                flash("Logged in successfully", category="success")
+                # login_user(user, remember=True)
+                return redirect(url_for("main.home"))
+            else:
+                flash("Incorrect password or email, please try again", category="error")
+        else:
+            flash("Email does not exist, please sign-up", category="error")
+
+    return render_template("login.html", user=current_user)
 
 
 @auth.route('/logout')
@@ -51,7 +66,7 @@ def sign_up():
 
             # login_user(new_user, remember=True)
             flash("Account created.", category="success")
-            return redirect(url_for("views.home"))
+            return redirect(url_for("main.home"))
         
     return render_template('sign_up.html')
 
